6de2cd5d82144309870c759d394538b10ff6f117
koreamarin <awldnjs2@gmail.com>
2023-07-25 02:31:48 +0900

    add 01977 완전제곱수

--- Java/src/beakjoon/BJ_01703_생장점_류지원.java
+++ Java/src/beakjoon/BJ_01703_생장점_류지원.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BJ_01703_생장점 {
+public class BJ_01703_생장점_류지원 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer ST;
--- Java/src/beakjoon/BJ_01977_완전제곱수_류지원.java
+++ Java/src/beakjoon/BJ_01977_완전제곱수_류지원.java
@@ -1,2 +1,39 @@
-package beakjoon;public class BJ_01977_완전제곱수_류지원 {
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
+public class BJ_01977_완전제곱수_류지원 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer ST = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(ST.nextToken());
+        ST = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(ST.nextToken());
+        ArrayList<Integer> A = new ArrayList();
+
+        for (int i = 1; i < 101; i++) {
+            if (N <= i * i && M >= i * i) {
+                A.add(i * i);
+            }
+        }
+        int sum = 0;
+        for(int i=0; i<A.size(); i++) {
+            sum+=A.get(i);
+        }
+
+        if (A.size()>0) {
+            System.out.println(sum);
+            System.out.print(A.get(0));
+        } else {
+            System.out.print(-1);
+        }
+
+
+    }
+
 }
