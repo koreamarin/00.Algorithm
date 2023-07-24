5e2e293f126e1f49b20c5e3b4d784695c0b7e796
koreamarin <awldnjs2@gmail.com>
2023-07-25 02:00:51 +0900

    add 01703 생장점

--- Java/src/beakjoon/BJ_01703_생장점.java
+++ Java/src/beakjoon/BJ_01703_생장점.java
@@ -1,2 +1,26 @@
-package beakjoon;public class BJ_01703_생장점 {
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_01703_생장점 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer ST;
+
+        while(true) {
+            ST = new StringTokenizer(br.readLine());
+            int r = 1;
+            int a = Integer.parseInt(ST.nextToken());
+            if(a==0) break;
+            for(int i=0; i<a; i++) {
+                int N = Integer.parseInt(ST.nextToken());
+                int M = Integer.parseInt(ST.nextToken());
+                r=(r*N)-M;
+            }
+            System.out.println(r);
+        }
+    }
 }
