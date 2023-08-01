dc74f359d755d2f633842a13eaf03e7328c6edaa
koreamarin <awldnjs2@gmail.com>
2023-08-01 19:27:23 +0900

    add SWEA D3 02805 농작물수확하기

--- Java/src/SWEA/D3_01208_SW문제해결기본1일차_Flatten.java
+++ Java/src/SWEA/D3_01208_SW문제해결기본1일차_Flatten.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class D3_1208_SW문제해결기본1일차_Flatten {
+public class D3_01208_SW문제해결기본1일차_Flatten {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer ST;
