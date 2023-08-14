66841a565186136f6f0ce068992aaf6f271d8043
koreamarin <awldnjs2@naver.com>
2023-08-14 17:59:26 +0900

    ssafy 0814

--- Java/src/beakjoon/BJ_01074_Z_류지원.java
+++ Java/src/beakjoon/BJ_01074_Z_류지원.java
@@ -3,52 +3,43 @@ package beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ_01074_Z_류지원 {
+	static int count=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer ST = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(ST.nextToken())+1;
+        int N = Integer.parseInt(ST.nextToken());
         int r = Integer.parseInt(ST.nextToken());
         int c = Integer.parseInt(ST.nextToken());
         int S = (int)Math.pow(2, N);
-        int[][] arr = new int[S][S];
-
-        counter(arr, N, 0);
-        System.out.println(S);
-
-
+        int[][] arr=new int[S][S];
+        int n = counter(S);
+        for(int i=0; i<S; i++) System.out.println(Arrays.toString(arr[i]));
     }
 
-    public static void counter(int[][] arr, int N, int count) {
-        N=N/2;
-        int[][] LTArr=new int[N][N];
-        int[][] RTArr=new int[N][N];
-        int[][] LBArr=new int[N][N];
-        int[][] RBArr=new int[N][N];
-        for(int i=0;i<N;i++) for(int j=0;j<N;j++) {
-            LTArr[i][j]=arr[i][j];
-            RTArr[i][j]=arr[i][j+N];
-            LBArr[i][j]=arr[i+N][j];
-            RBArr[i][j]=arr[i+N][j+N];
-        }
-        if(N==1) {
-            LTArr[0][0] = count++;
-            RTArr[0][0] = count++;
-            LBArr[0][0] = count++;
-            RBArr[0][0] = count++;
-            arr[N-1][N-1]=LTArr[0][0];
-            arr[N-1][N]=RTArr[0][0];
-            arr[N][N-1]=LBArr[0][0];
-            arr[N][N]=RBArr[0][0];
-        }
-
-        counter(LTArr, N, count);    // 왼위
-        counter(RTArr, N, count);    // 오위
-        counter(LBArr, N, count);    // 왼아
-        counter(RBArr, N, count);    // 오아
-
-
+//    public static int[][] counter(int[][] arr, int S) {
+//        if(S==2) return new int[][] {{count++,count++},{count++,count++}};
+//        int hs=S/2;
+//        int[][] LTArr=counter(new int[hs][hs], hs);    // 왼위
+//    	int[][] RTArr=counter(new int[hs][hs], hs);    // 오위
+//    	int[][] LBArr=counter(new int[hs][hs], hs);    // 왼아
+//    	int[][] RBArr=counter(new int[hs][hs], hs);    // 오아
+//        for(int i=0; i<hs; i++) {
+//        	for(int j=0; j<hs; j++) {
+//        		arr[i][j]=LTArr[i][j];
+//        		arr[i][j+S/2]=RTArr[i][j];
+//        		arr[i+S/2][j]=LBArr[i][j];
+//        		arr[i+S/2][j+S/2]=RBArr[i][j];
+//        	}
+//        }
+//        return arr;
+//    }
+    
+    public static int counter(int S) {
+    	if(S==1) return count++;
     }
+
 }
