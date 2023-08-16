70b7bb2a8379654ab15387d251fed8305bd7d760
koreamarin <awldnjs2@gmail.com>
2023-08-17 02:03:52 +0900

    add BJ 01074 Z

--- Java/src/beakjoon/BJ_01074_Z_류지원.java
+++ Java/src/beakjoon/BJ_01074_Z_류지원.java
@@ -6,54 +6,53 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * @author 류지원
+ * 메모리 : 11536KB
+ * 시간 : 76ms
+ * 풀이방법 :
+ * 분할 정복을 사용하였다.
+ * 다만, 배열을 사용하면 메모리초과, 브루트포스를 하면 시간초과가 걸리므로
+ * 아래적힌바와 같이 조건에 따라 conting을 해야한다.
+ * 가야하는 쪽이 1,2,3,4사분면 중에 어디인지를 판단 후, count를 한번에 더하는 방식을 사용하였다.
+ */
+
 public class BJ_01074_Z_류지원 {
-	static int count=0; static int i=0; static int j=0;
+	static int count=0; static int row; static int col;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer ST = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(ST.nextToken());
-        int r = Integer.parseInt(ST.nextToken());
-        int c = Integer.parseInt(ST.nextToken());
-        int S = (int)Math.pow(2, N);
-        int[][] arr=new int[S][S];
-        counter(S, 0);
-        for(int i=0; i<S; i++) System.out.println(Arrays.toString(arr[i]));
+        row = Integer.parseInt(ST.nextToken());
+        col = Integer.parseInt(ST.nextToken());
+        int S = (int)Math.pow(2, N);    // 크기
+        counter(0, 0, S);       // 분할정복 시작
+        System.out.println(count);      // 카운트 출력
     }
 
-//    public static int[][] counter(int[][] arr, int S) {
-//        if(S==2) return new int[][] {{count++,count++},{count++,count++}};
-//        int hs=S/2;
-//        int[][] LTArr=counter(new int[hs][hs], hs);    // 왼위
-//    	int[][] RTArr=counter(new int[hs][hs], hs);    // 오위
-//    	int[][] LBArr=counter(new int[hs][hs], hs);    // 왼아
-//    	int[][] RBArr=counter(new int[hs][hs], hs);    // 오아
-//        for(int i=0; i<hs; i++) {
-//        	for(int j=0; j<hs; j++) {
-//        		arr[i][j]=LTArr[i][j];
-//        		arr[i][j+S/2]=RTArr[i][j];
-//        		arr[i+S/2][j]=LBArr[i][j];
-//        		arr[i+S/2][j+S/2]=RBArr[i][j];
-//        	}
-//        }
-//        return arr;
-//    }
-    
-    public static void counter(int S, int num) {
-    	if(S==1) return;
+    public static void counter(int r, int c, int S) {       // 매개변수 r은 행의 시작점 위치, c은 열의 시작점 위치, S는 참조하고 있는 공간의 크기
+        // 기저조건. 계산 없음
+        if (S==1) return; 
 
-        if(num==1){
-            j+=1; i-=S/2;
-        } else if(num==2) {
-            j+=1;
-        } else if(num==3) {
-            i+=1; j-=S;
-        } else if(num==4) {
-            i+=1;
+        // 유도파트
+        int HS=S/2;                         // 참조하고 있는 공간의 절반 크기
+        int Sqr=HS*HS;                      // sqr는 count에 더해야하는 수를 계산하기 위한 용도
+        int NR=r+HS; int NC=c+HS;           // 새로 참조해야하는 공간의 행,열을 계산하기 위한 용도.
+
+        if(NR>row && NC>col) {              // 최종목적지가 참조하고 있는 공간의 2사분면에 있다면
+            counter(r,c,HS);                // 2사분면에 해당하는 위치로 이동, 참조하고 있는 크기가 절반이 되므로 HS를 넣음. 시작위치는 변하지 않으므로 r,c 그대로 넣음.
         }
-
-        counter(S/2,1);
-        counter(S/2,2);
-        counter(S/2,3);
-        counter(S/2,4);
+        else if(NR>row && NC<=col) {        // 최종목적지가 참조하고 있는 공간의 1사분면에 있다면
+            count+=Sqr;                     // 1사분면에 이동하기 전, 1사분면으로 시작위치로 이동했을시의 count 수를 더함.
+            counter(r,NC,HS);               // 1사분면에 해당하는 위치로 이동, 참조하고 있는 크기가 절반이 되므로 HS를 넣음. 시작위치는 열만 변하므로 NC를 넣음.
+        }
+        else if(NR<=row && NC>col) {        // 최종목적지가 참조하고 있는 공간의 3사분면에 있다면
+            count+=Sqr*2;                   // 3사분면에 이동하기 전, 3사분면으로 시작위치로 이동했을시의 count 수를 더함.
+            counter(NR, c, HS);             // 3사분면에 해당하는 위치로 이동, 참조하고 있는 크기가 절반이 되므로 HS를 넣음. 시작위치는 행만 변하므로 NR를 넣음.
+        }
+        else if(NR<=row && NC<=col) {       // 최종목적지가 참조하고 있는 공간의 4사분면에 있다면
+            count+=Sqr*3;                   // 4사분면에 이동하기 전, 4사분면으로 시작위치로 이동했을시의 count 수를 더함.
+            counter(NR, NC, HS);            // 4사분면에 해당하는 위치로 이동, 참조하고 있는 크기가 절반이 되므로 HS를 넣음. 시작위치는 행과 열이 변하므로 NR, NC를 넣음.
+        }
     }
 }
