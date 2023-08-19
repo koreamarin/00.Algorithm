251cf0100da214b3bb45d35fce1bf00db6072b64
koreamarin <awldnjs2@gmail.com>
2023-08-19 22:45:57 +0900

    add BJ 02615 오목, BJ 19539 사과나무

--- Java/src/beakjoon/BJ_02615_오목_류지원.java
+++ Java/src/beakjoon/BJ_02615_오목_류지원.java
@@ -15,43 +15,69 @@ public class BJ_02615_오목_류지원 {
             ST=new StringTokenizer(br.readLine());
             for(int j=0;j<19;j++) arr[i][j]=Integer.parseInt(ST.nextToken());
         }
-
         // 바둑판을 sweep하며 검은돌, 흰돌을 찾음
         for(int i=0;i<19;i++) {
             for(int j=0;j<19;j++) {
-                int arrValue = arr[i][j];
-                if(arrValue==1 || arrValue==2) {
-                    if (OverallVerification(i, j, arr)) System.out.println(arrValue+"\n"+i+" "+j);
+                if(arr[i][j]>0) {
+                    if(OverallVerification(i, j, arr)) {
+                        System.out.println(arr[i][j]+"\n"+(i+1)+" "+(j+1));
+                        return;
+                    }
                 }
             }
         }
+        System.out.println(0);
     }
 
     public static boolean OverallVerification(int i,int j, int[][] arr){
-        if (R_Verification(i, j, arr)) return true;
-        else if (B_Verification(i, j, arr)) return true;
-        else if (RB_Verification(i, j, arr)) return true;
-        else if (RT_Verification(i, j, arr)) return true;
-        return false;
-    }
-    public static boolean R_Verification(int i,int j, int[][] arr) {
-        int A=arr[i][j];
-        if(arr[i][j+1]==A && arr[i][j+2]==A && arr[i][j+3]==A && arr[i][j+4]==A) {
-            if(arr[i][j+5]==A) return false;
+        if (R_Verification(i, j, arr) ||
+            B_Verification(i, j, arr) ||
+            RB_Verification(i, j, arr) ||
+            RT_Verification(i, j, arr)) {
             return true;
         }
         return false;
     }
+    public static boolean R_Verification(int i,int j, int[][] arr) {
+        int A=arr[i][j];
+        if(j+4<19) {
+            if (arr[i][j+1]==A && arr[i][j+2]==A && arr[i][j+3]==A && arr[i][j+4]==A) {
+                if ((j+5<19 && arr[i][j+5]==A) || (j>0 && arr[i][j-1]==A)) return false;
+                return true;
+            }
+        }
+        return false;
+    }
     public static boolean B_Verification(int i,int j, int[][] arr) {
-
+        int A=arr[i][j];
+        if(i+4<19) {
+            if (arr[i + 1][j] == A && arr[i + 2][j] == A && arr[i + 3][j] == A && arr[i + 4][j] == A) {
+                if ((i+5<19 && arr[i+5][j]==A) || (i > 0 && arr[i - 1][j] == A)) return false;
+                return true;
+            }
+        }
         return false;
     }
     public static boolean RB_Verification(int i,int j, int[][] arr) {
-
+        int A=arr[i][j];
+        if(j+4<19 && i+4<19) {
+            if (arr[i + 1][j + 1] == A && arr[i + 2][j + 2] == A && arr[i + 3][j + 3] == A && arr[i + 4][j + 4] == A) {
+                if ((i+5<19 && j+5<19 && arr[i + 5][j + 5] == A) || (i > 0 && j>0 && arr[i - 1][j - 1] == A))
+                    return false;
+                return true;
+            }
+        }
         return false;
     }
-    public static boolean RT_Verification(int r,int c, int[][] arr) {
-        // 6개는 아닌지 확인해야함
+    public static boolean RT_Verification(int i,int j, int[][] arr) {
+        int A=arr[i][j];
+        if(j+4<19 && i-4>=0) {
+            if (arr[i - 1][j + 1] == A && arr[i - 2][j + 2] == A && arr[i - 3][j + 3] == A && arr[i - 4][j + 4] == A) {
+                if ((i-5>=0 && j+5<19 && arr[i - 5][j + 5] == A) || (i < 18 && j>0 && arr[i + 1][j-1] == A))
+                    return false;
+                return true;
+            }
+        }
         return false;
     }
 }
--- Java/src/beakjoon/BJ_19539_사과나무_류지원.java
+++ Java/src/beakjoon/BJ_19539_사과나무_류지원.java
@@ -1,21 +1,35 @@
 package beakjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * @author 류지원
+ * 메모리 : 21832KB
+ * 시간 : 212ms
+ * 풀이방법 :
+ * 대전제조건 : 1을 더한 횟수와 2을 더한 횟수가 같을 수 있다면 나무의 길이를 맞추는게 가능하다.
+ *
+ * 하위 전제조건 :
+ * 대 전제조건이 성립되려면 2가지의 하위 전제조건이 성립되면 된다.
+ * 1, 1을 더한 횟수와 2을 더한 횟수가 같아지게 만든다.
+ * 2. 1번 조건을 해냈을 때, 남은 목표길이의 합이 3의 배수라면 대 전제조건에 성립된다.
+ * 
+ * 구현 방법 : 
+ * 목표길이에서 홀수인 것을 찾은다음, 1을 더해야하는 횟수를 센다. 그 후, 2를 더해야 하는 횟수를 센다.
+ * 2를 더해야 하는 횟수에서 1을 더해야하는 횟수를 뺐을때 0이상이어야 한다. 음수라면 목표나무길이를 만들지 못하는 것이다.
+ * 2를 더해야 하는 횟수에서 1을 더해야하는 횟수를 뻈을때의 값을 3으로 나눴을 때, 값이 0이어야 한다. 그렇지 않다면 목표나무 길이를 만들지 못하는 것이다.
+ */
 public class BJ_19539_사과나무_류지원 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer ST;
-
-        int N=Integer.parseInt(br.readLine());
-        ST=new StringTokenizer(br.readLine());
-        int[] h=new int[N];
-        for(int i=0; i<N; i++) h[i]=Integer.parseInt(ST.nextToken());
-
-        //
-
+        int N=Integer.parseInt(br.readLine()); int plus1=0, plus2=0;
+        StringTokenizer ST=new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int A=Integer.parseInt(ST.nextToken());
+            if(A%2==1) plus1+=1;    // 1을 더해야 하는 횟수 세기
+            plus2+=A/2;             // 2를 더해야 하는 횟수 세기
+        }
+        System.out.println((plus2>=plus1 && (plus2-plus1)%3==0)? "YES" : "NO");   // 나머지가 3의 배수인지 확인하기.
     }
 }
