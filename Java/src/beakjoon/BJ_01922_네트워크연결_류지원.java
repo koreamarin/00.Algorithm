643aa6dec3aaad9ccb3e6ee6c16d35485fa0692a
KoreaMarin <awldnjs2@naver.com>
2023-09-06 17:58:34 +0900

    ssafy 230906

---
+++ Java/src/beakjoon/BJ_01922_네트워크연결_류지원.java
@@ -0,0 +1,23 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_01922_네트워크연결_류지원 {
+	
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer ST;
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		for(int i=0; i<M; i++) {
+			ST=new StringTokenizer(br.readLine());
+			
+		}
+
+	}
+
+}
