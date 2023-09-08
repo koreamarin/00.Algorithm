67db527f93a40474b6041269fcdc76ca3638692a
KoreaMarin <awldnjs2@naver.com>
2023-09-08 17:59:41 +0900

    ssafy 230908

---
+++ Java/src/beakjoon/BJ_18352_특정거리의도시찾기_류지원.java
@@ -0,0 +1,32 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ_18352_특정거리의도시찾기_류지원 {
+
+	static class Node {
+		int vertex;
+		Node next;
+		
+		public Node(int vertex, Node next) {
+			this.vertex = vertex;
+			this.next = next;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer ST;
+		
+		ArrayList<Node> Nodes = new ArrayList<>();
+		
+		Node[] A = new Node()[]
+		int[] a = new int 
+		
+	}
+
+}
