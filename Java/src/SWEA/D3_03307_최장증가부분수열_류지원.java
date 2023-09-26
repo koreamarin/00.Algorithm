c87560628d233decc81abb1d8dc089df02a5e25e
koreamarin <awldnjs2@gmail.com>
2023-09-27 03:19:32 +0900

    add BJ 2239 스도쿠, D3 3307 최장증가부분수열

--- Java/src/SWEA/D3_03307_최장증가부분수열_류지원.java
+++ Java/src/SWEA/D3_03307_최장증가부분수열_류지원.java
@@ -1,20 +1,48 @@
 package SWEA;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @author 류지원
+ * 시간 : 147ms
+ * 메모리 : 27740kb
+ * 
+ * 풀이방법 : DP를 사용하였다.
+ */
 
 public class D3_03307_최장증가부분수열_류지원 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T=Integer.parseInt(br.readLine());
-		for(int t=0; t<T; t++) {
-			int N=Integer.parseInt(br.readLine());
-		}
-		
-		
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer ST;
+        StringBuffer sb = new StringBuffer();
+        
+        int T = Integer.parseInt(br.readLine());
+        for (int t=1; t<=T; t++) {
+        	int N=Integer.parseInt(br.readLine());
 
-	}
+            int[] arr = new int[N];
+            int[] dp = new int[N];
 
+            // 수열 입력받기
+            ST=new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(ST.nextToken());
+            
+            int max=0;
+            Arrays.fill(dp, 1);							// 초기값, dp의 모든 원소에 전부 1 할당
+            
+            for (int i=1; i<N; i++) {
+                for(int j=0; j<i; j++) { 				// 현재 참조하고있는 i보다 원소번호가 작은 곳까지 dp를 순회하기 위한 반복문
+                    if (arr[j]<arr[i]) 					// 현재 참조하고 있는 dp[i]의 원소값이 i이하의 원소들보다 클때,
+                    	if(dp[i]<=dp[j]) 				// 전 dp값이 현재 dp값보다 크거나 같으면 (즉, 갱신되고 있는 현재 dp가 전 dp보다 작다면)
+                    		dp[i]=dp[j]+1;				// dp[i]는 전dp값인 dp[j]보다 +1 큰 수를 할당
+                }
+                if(dp[i]>max) max=dp[i];		// dp의 모든 원소중 가장 큰 수를 max에 저장
+            }
+            System.out.println("#" + t + " " + max);
+        }
+    }
 }
--- Java/src/test.java
+++ Java/src/test.java
@@ -2,14 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class test {
     public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer ST=new StringTokenizer(br.readLine());
-		System.out.println(ST.countTokens());
+//		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer ST=new StringTokenizer(br.readLine());
+//		System.out.println(ST.countTokens());
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1); list.add(2);
+		
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
 
     }
 }
