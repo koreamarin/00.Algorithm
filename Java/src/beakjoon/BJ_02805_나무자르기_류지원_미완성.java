96cab7dc557d39616f7a9eab346d9d636cd412df
koreamarin <awldnjs2@gmail.com>
2024-07-19 19:37:03 +0900

    BJ_02805_나무자르기

--- Java/src/beakjoon/BJ_02805_나무자르기_류지원_미완성.java
+++ Java/src/beakjoon/BJ_02805_나무자르기_류지원_미완성.java
@@ -5,29 +5,74 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @author "Ryu jiwon"
+ * 시간 : 
+ * 메모리 : 
+ * 풀이방법 : 
+ * 나무의 높이가 10억까지 있으므로 이분탐색이 필요하다.
+ * 
+ */
+
+
+
 public class BJ_02805_나무자르기_류지원_미완성 {
+	static int N, M, MaxHeight;
+	static int[] trees;
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer ST = new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(ST.nextToken()), M=Integer.parseInt(ST.nextToken());
-        int[] trees=new int[N];
+        N=Integer.parseInt(ST.nextToken());
+        M=Integer.parseInt(ST.nextToken());
+        trees=new int[N];
         ST=new StringTokenizer(br.readLine());
-        int MaxLength=0;
+        
         for(int i=0; i<N; i++) {
             trees[i]=Integer.parseInt(ST.nextToken());
-            MaxLength=Math.max(MaxLength, trees[i]);
+            MaxHeight = Math.max(MaxHeight, trees[i]);
         }
+        int add = MaxHeight/2;
+		System.out.println(binarySearch(MaxHeight/2, add/2));
+    }
+    
+    public static int binarySearch(int h, int add){
+    	// 기저조건
+    	int sum=0;
+    	int sum1=0;
+    	int sum2=0;
+    	
+//    	try {
+//			Thread.sleep(500);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+    	
+    	for(int tree:trees) {
+    		if(tree>h) {
+    			sum += tree-h;
+    			sum1 += tree-(h-1);
+    			sum2 += tree-(h+1);
+    		}
+    	}
 
-
-        for(int i=0; i<MaxLength; i++) {
-            int sum=0;
-            for(int tree : trees) {
-                if(i<tree) sum += tree - i;
-            }
-            if(sum<=M) {
-                System.out.println(i = (i==0) ? i : i-1);
-                break;
-            }
-        }
+    	System.out.println(h + " " + sum + " " + sum1 + " " + sum2);
+    	
+    	if(sum==M) {
+    		return h;
+    	}
+    	else if(sum<M && sum1>=M) {
+    		return h-1;
+    	}
+    	else if(sum>M && sum2<=M) {
+    		return h;
+    	}
+    	// 유도파트
+    	else if(sum<M) 
+    		return binarySearch(h-add, add/2);
+    	else // sum>M
+    		return binarySearch(h+add, add/2);
+    	
     }
 }
