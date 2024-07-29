099cf95a13a07a98ccaefc37074b8d2a59b1e6aa
koreamarin <awldnjs2@gmail.com>
2024-07-29 21:44:42 +0900

    BJ_01991_트리순회

--- Java/src/beakjoon/BJ_01991_트리순회_류지원.java
+++ Java/src/beakjoon/BJ_01991_트리순회_류지원.java
@@ -5,14 +5,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ_01991_트리순회_류지원 {
-	static Node head = new Node('A', null, null); // 루트노드
+	static Node head = new Node('A', null, null);
 	
-	static class Node {
+	public static class Node{
 		char value;
 		Node left;
 		Node right;
-		
-		public Node(char value, Node left, Node right) {
+		Node(char value, Node left, Node right) {
 			this.value = value;
 			this.left = left;
 			this.right = right;
@@ -21,61 +20,52 @@ public class BJ_01991_트리순회_류지원 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer ST;
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            char root = st.nextToken().charAt(0);
-            char left = st.nextToken().charAt(0);
-            char right = st.nextToken().charAt(0);
-
-            insertNode(head, root, left, right);
-        }
-
-        preOrder(head);
-        System.out.println();
-        inOrder(head);
-        System.out.println();
-        postOrder(head);
-        System.out.println();
+		for(int n=0; n<N; n++) {
+			ST=new StringTokenizer(br.readLine());
+			char value = ST.nextToken().charAt(0);
+			char left = ST.nextToken().charAt(0);
+			char right = ST.nextToken().charAt(0);
+			insertNode(head, value, left, right);
+		}
+		preOrder(head);
+		System.out.println();
+		inOrder(head);
+		System.out.println();
+		postOrder(head);
+		System.out.println();
 	}
 	
-	public static void insertNode(Node tmp, char root, char left, char right) {
-        if (tmp.value == root) {
-            tmp.left = (left == '.' ? null : new Node(left, null, null));
-            tmp.right = (right == '.' ? null : new Node(right, null, null));
-        } else {
-            if (tmp.left != null) {
-                insertNode(tmp.left, root, left, right);
-            }
-            if (tmp.right != null) {
-                insertNode(tmp.right, root, left, right);
-            }
-        }
-    }
+	public static boolean insertNode(Node node, char value, char left, char right) {
+		if(node.value==value) {	// 기저조건
+			node.left = left == '.' ? null : new Node(left, null, null);
+			node.right = right == '.' ? null : new Node(right, null, null);
+			return true;
+		}
+		// 유도파트
+		if(node.left!=null && insertNode(node.left,  value, left, right)) 		return true;
+		else if(node.right!=null && insertNode(node.right, value, left, right))	return true;
+		return false;
+	}
 	
-	private static void preOrder(Node node) {
-        if (node == null) return;
-        System.out.print(node.value);
-        preOrder(node.left);
-        preOrder(node.right);
-    }
-
-    private static void inOrder(Node node) {
-        if (node == null) return;
-        inOrder(node.left);
-        System.out.print(node.value);
-        inOrder(node.right);
-    }
-
-    private static void postOrder(Node node) {
-        if (node == null)return;
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.print(node.value);
-    }
-
-
+	private static void preOrder(Node node) {	// 전위순회
+		System.out.print(node.value);
+		if(node.left!=null) preOrder(node.left);
+		if(node.right!=null) preOrder(node.right);
+	}
+	
+	private static void inOrder(Node node) {	// 중위순회
+		if(node.left!=null) inOrder(node.left);
+		System.out.print(node.value);
+		if(node.right!=null) inOrder(node.right);
+	}
+	
+	private static void postOrder(Node node) {	// 후위순회
+		if(node.left!=null) postOrder(node.left);
+		if(node.right!=null) postOrder(node.right);
+		System.out.print(node.value);
+	}
 }
